ad09e48a046c1e4930ce6a295c921f8ee0adf17f
ampotos <mercie_i@epitech.eu>
2015-04-30 15:46:01 +0200

    instruction now correctly get in r/w monitoring

--- src/rw.c
+++ src/rw.c
@@ -6,33 +6,48 @@
 
 // todo  : find a way to get instruction with pc (and if none do nothing)
 
-void	memory_read(app_pc pc)
+void	memory_read(void *pc)
 {
-  instr_t	instr;
   void		*drcontext = dr_get_current_drcontext();
+  instr_t	*instr = instr_create(drcontext);
   opnd_t	src;
 
   pc = dr_app_pc_for_decoding(pc);
-  decode(drcontext, pc, &instr);
 
-  for (int i = 0; i < instr_num_srcs(&instr); i++)
+  instr_init(drcontext, instr);
+  if (!decode(drcontext, pc, instr))
     {
-      src = instr_get_src(&instr, i);
+      dr_printf("Decode of instruction at %p failed\n", pc);
+      return;
     }
+
+  for (int i = 0; i < instr_num_srcs(instr); i++)
+    {
+      src = instr_get_src(instr, i);
+    }
+
+  instr_destroy(drcontext, instr);
 }
 
-void	memory_write(app_pc pc)
+void	memory_write(void *pc)
 {
-  instr_t	instr;
   void		*drcontext = dr_get_current_drcontext();
+  instr_t	*instr = instr_create(drcontext);
   opnd_t	dst;
-
   
   pc = dr_app_pc_for_decoding(pc);
-  decode(drcontext, pc, &instr);
 
-  for (int i = 0; i < instr_num_dsts(&instr); i++)
+  instr_init(drcontext, instr);
+  if(!decode(drcontext, pc, instr))
     {
-      dst = instr_get_dst(&instr, i);
+      dr_printf("Decode of instruction at %p failed\n", pc);
+      return;
     }
+
+  for (int i = 0; i < instr_num_dsts(instr); i++)
+    {
+      dst = instr_get_dst(instr, i);
+    }
+
+  instr_destroy(drcontext, instr);
 }

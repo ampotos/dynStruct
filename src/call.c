4a0f760ca0147d5e3215ee0a18e60abf516947e6
ampotos <mercie_i@epitech.eu>
2015-05-15 17:54:24 +0200

    add call.c on the CMakeList

--- src/call.c
+++ src/call.c
@@ -3,6 +3,7 @@
 void	dir_call_monitor(void *pc)
 {
   // TODO get the addr off the caled function and push it to stack (with global tls index)
+  dr_printf("direct call\n");
   return;
 }
 
@@ -10,6 +11,7 @@ void	dir_call_monitor(void *pc)
 void	ind_call_monitor(void *pc)
 {
   // TODO get the addr off the caled function and push it to stack (with global tls index)
+  dr_printf("indirect call\n");
   return;
 }
 
@@ -17,5 +19,6 @@ void	ind_call_monitor(void *pc)
 void	ret_monitor(void *pc)
 {
   // TODO pop addr from tls stack (maybe problematic if nb call/ret differ so chack this correctly
+  dr_printf("return\n");
   return;
 }

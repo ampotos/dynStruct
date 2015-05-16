052d7d2c433617d80d72544b25f4e7257bdc7950
ampotos <mercie_i@epitech.eu>
2015-05-16 14:10:36 +0200

    correction fail size calcul for calloc

--- src/call.c
+++ src/call.c
@@ -3,7 +3,6 @@
 void	dir_call_monitor(void *pc)
 {
   // TODO get the addr off the caled function and push it to stack (with global tls index)
-  dr_printf("direct call\n");
   return;
 }
 
@@ -11,14 +10,12 @@ void	dir_call_monitor(void *pc)
 void	ind_call_monitor(void *pc)
 {
   // TODO get the addr off the caled function and push it to stack (with global tls index)
-  dr_printf("indirect call\n");
   return;
 }
 
 
 void	ret_monitor(void *pc)
 {
-  // TODO pop addr from tls stack (maybe problematic if nb call/ret differ so chack this correctly
-  dr_printf("return\n");
+  // TODO pop addr from tls stack 
   return;
 }

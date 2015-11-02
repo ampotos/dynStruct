242062485f24d63686546c6afa1a18f3ccae70e7
ampotos <mercie_i@epitech.eu>
2015-11-02 17:06:25 +0000

    fix warning in build.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ rm -rf build
 mkdir build
 cd build
 
-if [ $1 -eq "32" ]
+if [ $# -eq 1 ] && [ $1 -eq 32 ]
 then
     CFLAGS="-W -Wall -Wextra -std=gnu99 -nostdlib -fno-builtin -O3 -m32" CXXFLAGS=-m32 cmake -DDynamoRIO_DIR=$DYNAMORIO_HOME/cmake ../ && make && cp libdynStruct.so ../dynStruct
 else

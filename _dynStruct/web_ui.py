648f74a7ce630497a29bcc9c7d5b3ca8a41725fb
ampotos <mercie_i@epitech.eu>
2016-01-22 16:46:54 +0000

    add todo directory with idea for the future of dynStruct, files for bottle template(empty for now)

--- _dynStruct/web_ui.py
+++ _dynStruct/web_ui.py
@@ -1,15 +1,13 @@
 import os
-from bottle import route
-from bottle import run
-from bottle import static_file
+import bottle
 
-@route("/")
+@bottle.route("/")
 def index():
-    return serve_static("index.html")
+    return bottle.serve_static("index.html")
 
-@route("/static/<filename>")
+@bottle.route("/static/<filename>")
 def serve_static(filename):
-    return static_file(filename, root=os.path.join(os.path.dirname(__file__), 'static'))
+    return bottle.static_file(filename, root=os.path.join(os.path.dirname(__file__), 'static'))
 
 def start_webui(l_struct, l_block, l_access_w, l_access_r, addr, port):
-    run(host=addr, port=port)
+    bottle.run(host=addr, port=port)

572d48017ffea9f4ba1660e22073debd135da8d5
ampotos <mercie_i@epitech.eu>
2016-01-25 19:36:54 +0000

    view of webui created (but empty), future_work update

--- _dynStruct/web_ui.py
+++ _dynStruct/web_ui.py
@@ -3,11 +3,13 @@ import bottle
 
 @bottle.route("/")
 def index():
-    return bottle.serve_static("index.html")
+    return bottle.template("index")
 
 @bottle.route("/static/<filename>")
 def serve_static(filename):
-    return bottle.static_file(filename, root=os.path.join(os.path.dirname(__file__), 'static'))
+    return bottle.static_file(filename, root=os.path.join(os.path.dirname(__file__), "static"))
 
 def start_webui(l_struct, l_block, l_access_w, l_access_r, addr, port):
+    bottle.TEMPLATE_PATH.insert(0, os.path.dirname(__file__) + "/views")
+    print(bottle.TEMPLATE_PATH)
     bottle.run(host=addr, port=port)

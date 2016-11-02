05fae306c3bdb678393363c75512f85c25bfcba3
ampotos <mercie_i@epitech.eu>
2016-11-02 16:21:51 +0100

    fix compile with last dynamorio build

--- includes/custom_alloc.h
+++ includes/custom_alloc.h
@@ -2,12 +2,19 @@
 #define CUSTOM_ALLOC_H_
 
 #include "allocs.h"
-
+#include <dr_tools.h>
 typedef struct page_header_s
 {
   void	*next_page;
   int	next_idx;
 } page_header_t;
+
+// dynamoRIO now redefine PAGE_SIZE here it's just not define
+// So if it does not exist just use the most common value of
+// today page size
+#ifndef PAGE_SIZE
+#define PAGE_SIZE 4096
+#endif
 
 #define MAX_IDX_ACCESS (PAGE_SIZE - sizeof(page_header_t)) / sizeof(access_t)
 #define MAX_IDX_ORIG (PAGE_SIZE - sizeof(page_header_t)) / sizeof(orig_t)

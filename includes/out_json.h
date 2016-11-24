0879b1d50377f676672e8a6a615510ccc867533b
ampotos <mercie_i@epitech.eu>
2016-11-24 17:11:09 +0100

    Disabling buffering On my setup the buffering is not properly working when analyzing "xterm -e 'exit'". A part of the json data are missing. Buffering will be re-enable when the issue will be fixed

--- includes/out_json.h
+++ includes/out_json.h
@@ -13,17 +13,20 @@ extern char    *tmp_buf;
 extern int     global_idx;
 extern int     len_tmp;
 
+// The buffering system there have one issue in my setup for xterm so
+// it's disable unitl it's fix
+#define DS_PRINTF(...) dr_fprintf(args->file_out,  __VA_ARGS__)
 //here macro is used instead of a function to avoid exposing va_list
-#define DS_PRINTF(...) {if (!global_buf || !tmp_buf)	                \
-    init_buffering();							\
-  len_tmp = dr_snprintf(tmp_buf, TMP_BUF_SIZE, __VA_ARGS__);		\
-  if (global_idx + len_tmp + 1 >= GLOBAL_BUF_SIZE)			\
-    {									\
-      dr_write_file(args->file_out, global_buf, global_idx);		\
-      global_idx = 0;							\
-    }									\
-  ds_memcpy(global_buf + global_idx, tmp_buf, len_tmp);			\
-  global_idx += len_tmp;}
+/* #define DS_PRINTF(...) {if (!global_buf || !tmp_buf)	                \ */
+/*     init_buffering();							\ */
+/*   len_tmp = dr_snprintf(tmp_buf, TMP_BUF_SIZE, __VA_ARGS__);		\ */
+/*   if (global_idx + len_tmp + 1 >= GLOBAL_BUF_SIZE)			\ */
+/*     {									\ */
+/*       dr_write_file(args->file_out, global_buf, global_idx);		\ */
+/*       global_idx = 0;							\ */
+/*     }									\ */
+/*   ds_memcpy(global_buf + global_idx, tmp_buf, len_tmp);			\ */
+/*   global_idx += len_tmp;} */
 
 void write_json(void);
 void flush_old_block(void);
